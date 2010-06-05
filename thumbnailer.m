71b8781457344d5d37417f0f8da2cf23d611d284
Dougal MacPherson <hello@newfangled.com.au>
2010-06-05 19:40:40 +1000

    Now accepts the path of the file to process. It's only one argument for now. Next is the destination file

--- thumbnailer.m
+++ thumbnailer.m
@@ -15,6 +15,24 @@
 #import <AppKit/NSBitmapImageRep.h>
 
 
+/*
+
+ 
+ Arguments for qlmanage (ammended)
+ We should try and follow these where possible
+ 
+ -h		Display this help
+ -r		Force reloading Generators list
+ -p		Compute previews of the documents
+ -t		Compute thumbnails of the documents
+ -i		Compute thumbnail in icon mode
+ -s size		Size for the thumbnail
+ -f factor	Scale factor for the thumbnail
+ -o dir		Output result in dir (don't display thumbnails or previews)
+ 
+ 
+ */
+
 int main (int argc, const char * argv[]) {
     
 	
@@ -25,10 +43,12 @@ int main (int argc, const char * argv[]) {
 	
 	// Filemanager - Who will check, open and save files
 	NSFileManager		*fm; 
-	NSString			*inFileName = @"assets/example.pdf";
-	NSString			*outFileName = @"thumbs/thumb.png";
-	NSString			*pwd;
+	NSString			*inFileName, *outFileName
 	NSFileHandle		*outFile;
+
+	// For looking up arguments supplied to the application
+	NSProcessInfo		*proc = [NSProcessInfo processInfo];
+	NSArray				*args = [proc arguments];
 	
 	// Quicklook stuff
 	NSURL				*inFileURL;
@@ -39,20 +59,29 @@ int main (int argc, const char * argv[]) {
 
 	
 	fm = [[NSFileManager alloc] init];	// Create an instance of the filemanager
-	pwd = [ fm currentDirectoryPath];	// Our current directory
-	inFileURL = [ NSURL fileURLWithPath: [pwd stringByAppendingPathComponent: inFileName]];	// Build url to file
 	
-	// Check that the file exists
-	if ([ fm fileExistsAtPath: inFileName] == NO) {
-		NSLog(@"File doesn't exist!");
+	//argv < 2
+	if ([args count] < 2 ) {
+		NSLog(@"No file supplied");
 		return result;
 	}
 	
+	inFileName = [args objectAtIndex: 1];
 	
+	// Check that the file exists
+	if ([ fm isReadableFileAtPath: inFileName] == NO) {
+		NSLog(@"Can't read %@", inFileName);
+		return result;
+	}
+	
+	inFileURL = [ NSURL fileURLWithPath: inFileName];	// Build url to file
+
 
 	qlThumb = QLThumbnailImageCreate(nil, (CFURLRef)inFileURL, CGSizeMake(800, 800), nil);	// Ask Quicklook for a representation of the file
 
 	if (qlThumb != nil) {
+		
+		outFileName = [ inFileName stringByAppendingString:@".png" ];
 		
 		[fm createFileAtPath: outFileName contents:nil attributes:nil];						// Create the outFile if required
 		outFile = [ NSFileHandle fileHandleForWritingAtPath: outFileName];					// Create handle to the outFileName
@@ -78,3 +107,4 @@ int main (int argc, const char * argv[]) {
     [pool drain];
     return result;
 }
+
